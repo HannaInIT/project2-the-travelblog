88c63560f72b50dab4d822bd7604ba951c39b0fb
HannaInIT <ann10062016@gmail.com>
2023-09-06 12:47:07 +0200

    change nav bar, home page, styles

---
+++ config/session.configuration.js
@@ -0,0 +1,20 @@
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/project2-the-travelblog"
+
+module.exports = app => {
+    // required for the app when deployed to Heroku (in production)
+    app.set('trust proxy', 1);
+
+    app.use(
+        session({
+            secret: process.env.SESSION_SECRET || "super hyper secret key",
+            resave: false,
+            saveUninitialized: false,
+            store: MongoStore.create({
+                mongoUrl: MONGO_URI,
+            }),
+        })
+    );
+};
